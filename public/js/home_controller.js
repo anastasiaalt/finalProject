c0cb053a04452f6f0a758c70a6905a30014a6395
Anastasia Alt <anastasia.alt@gmail.com>
2016-01-20 13:21:25 -0500

    initial file structure

---
+++ public/js/home_controller.js
@@ -0,0 +1,32 @@
+angular.module('StressApp').controller('HomeController', HomeController)
+
+HomeController.$inject = ['$http'];
+
+function HomeController($http){
+
+    var problems = this;
+
+    problems.all = [];
+
+
+    problems.add = function(){
+
+    };
+
+
+    problems.comment = function(){
+
+    };
+
+
+    problems.upvote = function(){
+
+    };
+
+
+    problems.show = function(){
+
+    };
+
+
+}
