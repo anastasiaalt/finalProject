f77ac1b3b636ea576adc6d39dd65f482f030c56c
Anastasia Alt <anastasia.alt@gmail.com>
2016-01-23 16:21:26 -0500

    topics posting

--- models/post.js
+++ models/post.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose');
 // TODO should this be associted with someone? or totes anonymous?
 var postSchema = mongoose.Schema({
     content: String,
-    topic: String
+    topic: String,
+    created: {type: Date, default: Date.now}
 });
 
 module.exports = mongoose.model('Post', postSchema);
